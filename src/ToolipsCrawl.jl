2e40a476696a8a02c428d66935100711c5fe3208
emmy <52672675+emmaccode@users.noreply.github.com>
2023-11-11 12:31:33 -0700

    Better `async` scraping :)

--- src/ToolipsCrawl.jl
+++ src/ToolipsCrawl.jl
@@ -1,5 +1,6 @@
 module ToolipsCrawl
 using Toolips
+using Toolips.Crayons
 import ToolipsSession: htmlcomponent, kill!
 
 mutable struct Crawler
@@ -26,19 +27,27 @@ function crawl!(crawler::Crawler)
     scrape!(crawler)
     allprops = findall(c::Component{<:Any} -> "href" in keys(c.properties), crawler.components)
     [begin
+        println("hello")
         comp = crawler.components[prop]
         lnk = comp["href"]
-        push!(crawler.addresses, comp)
+        push!(crawler.addresses, comp["href"])
     end for prop in allprops]
-    return
 end
 
 function crawl(f::Function, address::String)
     crawler::Crawler = Crawler(address)
     crawler.crawling = true
-    Threads.@spawn while crawler.crawling
+    println(Crayon(foreground = Symbol("light_magenta"), bold = true), "Crawler: crawler started at $address")
+    @async while crawler.crawling
         crawl!(crawler)
         f(crawler)
+        if length(crawler.addresses) < 1
+            crawler.crawling = false
+            println(Toolips.Crayon())
+            break
+        end
+        crawler.address = crawler.addresses[1]
+        deleteat!(crawler.addresses, 1)
     end
     crawler::Crawler
 end
@@ -53,7 +62,10 @@ function scrape(f::Function, address::String, components::String ...)
     f(scrape!(crawler, [components ...]))
 end
 
-kill!(crawler::Crawler) = crawler.crawling = false
+kill!(crawler::Crawler) = begin
+    crawler.crawling = false
+    println(Crayon(foreground = Symbol("light_red"), bold = true, blink = true), "Crawler: crawler stopped")
+end
 
 export scrape, crawl, kill
 
